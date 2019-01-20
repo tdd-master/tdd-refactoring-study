09eb4b8e4934b27d287f9e970aa52f28b0b98188
Hosik <>
2019-01-20 22:47:14 +0900

    basic calculator

--- tdd-study/src/main/java/com/hosik/calculator/Calculator.java
+++ tdd-study/src/main/java/com/hosik/calculator/Calculator.java
@@ -1,7 +1,11 @@
 package com.hosik.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     private String seperator;
+
     public Calculator() {
         seperator = ",";
     }
@@ -14,8 +18,26 @@ public class Calculator {
         return seperator;
     }
 
-    public void calc(String input) {
+    public Integer calc(String input) {
+        List<Integer> numbers = seperate(input);
+        int result = 0;
+        for (Integer num : numbers) {
+            result += num;
+        }
+        return result;
+    }
 
+    private List<Integer> seperate(String input) {
+        String [] numString = input.split(seperator);
+        List<Integer> numbers = getIntegers(numString);
+        return numbers;
+    }
 
+    private List<Integer> getIntegers(String[] numString) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String num : numString) {
+            numbers.add(Integer.parseInt(num));
+        }
+        return numbers;
     }
 }
