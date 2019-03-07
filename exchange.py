4daf4a6509fb8238b7672de3b6b4f2a20426a421
hjk <soeque1@gmail.com>
2019-03-07 20:53:38 +0900

    Ch4
    amount를 private으로 만들기

--- exchange.py
+++ exchange.py
@@ -1,10 +1,13 @@
 class Dollar:
     def __init__(self, amount):
-        self.amount = amount
+        self.__amount = amount
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Dollar(self.__amount * multiplier)
 
     def equals(self, object):
         dollar = object
-        return self.amount == dollar.amount
+        return self.__amount == dollar.__amount
+
+    def __eq__(self, other):
+        return self.__amount == other.__amount
--- tests/test_currency.py
+++ tests/test_currency.py
@@ -4,10 +4,8 @@ from exchange import Dollar
 class TesCurrency(unittest.TestCase):
     def test_multiplication(self):
         five = Dollar(5)
-        product = five.times(2)
-        self.assertEqual(10, product.amount)
-        product = five.times(3)
-        self.assertEqual(15, product.amount)
+        self.assertEqual(Dollar(10), five.times(2))
+        self.assertEqual(Dollar(15), five.times(3))
 
     def test_equality(self):
         self.assertTrue(Dollar(5).equals(Dollar(5)))
