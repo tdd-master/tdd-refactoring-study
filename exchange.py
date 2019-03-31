a49a69b55609e0642ae5c3a724db29fce28c43c4
soeque <soeque1@gmail.com>
2019-03-31 16:15:24 +0900

    Ch14. 바꾸기

--- exchange.py
+++ exchange.py
@@ -42,23 +42,49 @@ class Money(Expression):
     def plus(self, addend):
         return Sum(self, addend)
 
-    def reduce(self, _to):
-        return self
+    def reduce(self, bank, _to):
+        rate = bank.rate(self._currency, _to)
+        return Money(self._amount / rate, _to)
 
 class Sum:
     def __init__(self, augend, addend):
         self.augend = augend
         self.addend = addend
 
-    def reduce(self, _to):
+    def reduce(self, bank, _to):
         amount = self.augend._amount + self.addend._amount
         return Money(amount, _to)
 
+class Pair:
+    def __init__(self, _from, _to):
+        self._from = _from
+        self._to = _to
+        self._dict = {_from, _to}
+
+    def getDict(self):
+        return str(self._dict)
+
+    ## TODO: Convert str to hash
+    def __hash__(self):
+        return 0
+
+    def equals(self, object):
+        pair = object
+        return pair._from == pair._to
 
 class Bank:
+    def __init__(self):
+        self._rates = {}
+
     def reduce(self, _source, _to):
-        if isinstance(_source, Money):
-            return _source.reduce(_to)
-        _sum = _source
-        return _sum.reduce(_to)
+        return _source.reduce(self, _to)
+
+    def rate(self, _from, _to):
+        if (_from == _to):
+            return 1
+        return self._rates.get(Pair(_from, _to).getDict())
+
+    def addRate(self, _from, _to, rate):
+        self._rates.update({Pair(_from, _to).getDict():rate})
+
 
--- tests/test_currency.py
+++ tests/test_currency.py
@@ -46,5 +46,17 @@ class TesCurrency(unittest.TestCase):
         result = bank.reduce(Money.dollar(1), "USD")
         self.assertEqual(Money.dollar(1), result)
 
+    def test_reduce_money_difference_currency(self):
+        bank = Bank()
+        bank.addRate("CHF", "USD", 2)
+        result = bank.reduce(Money.franc(2), "USD")
+        self.assertEqual(Money.dollar(1), result)
+
+    def test_array_equals(self):
+        pass
+
+    def test_identity_rate(self):
+        self.assertEqual(1, Bank().rate("USD", "USD"))
+
 if __name__ == '__main__':
     unittest.main()
